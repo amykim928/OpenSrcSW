37d15392b1b89d8efc412c2179b2a76286a3fc8a
amykim928 <hyeonah928@gmail.com>
2021-03-20 06:11:26 +0900

    3주차 구동클래스 main class

---
+++ Assignment/src/javaAssignment/Kuir.java
@@ -0,0 +1,24 @@
+package javaAssignment;
+
+public class Kuir {
+
+	public static void main(String[] args){
+		System.out.println("args0: " + args[0]);
+		System.out.println("args1: " + args[1]);
+		// TODO Auto-generated method stub
+
+		if(args[0].equals("-c")) {
+			String htmlFileName = args[1];
+			MakeCollection mc = new MakeCollection();
+			mc.convertHTMLtoXML(htmlFileName);
+		}
+		else if(args[0].equals("-k")) {
+			String xmlFileName = args[1];
+			MakeKeyword mk = new MakeKeyword();
+			mk.main(xmlFileName);
+		} else {
+			System.out.println("Invalid argument.");
+		}
+	}
+
+}
